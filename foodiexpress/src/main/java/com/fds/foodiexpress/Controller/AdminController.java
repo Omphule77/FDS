c7d880762624bd20263e8bcab8fc388b63217945
lalit-shelkar <lalitshelkar2424@gmail.com>
2025-06-10 13:34:20 +0530

    admin operationsc

--- foodiexpress/src/main/java/com/fds/foodiexpress/Controller/AdminController.java
+++ foodiexpress/src/main/java/com/fds/foodiexpress/Controller/AdminController.java
@@ -5,8 +5,10 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,18 +17,24 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fds.foodiexpress.Service.AdminService;
 import com.fds.foodiexpress.entity.Admin;
+import com.fds.foodiexpress.entity.Authorities;
 import com.fds.foodiexpress.entity.Customer;
 import com.fds.foodiexpress.entity.Delivery;
 import com.fds.foodiexpress.entity.Restaurant;
+import com.fds.foodiexpress.entity.Users;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class AdminController {
 
 	private final AdminService adminService;
+	private PasswordEncoder passwordEncoder;
 
 	@Autowired
-	public AdminController(AdminService adminService) {
+	public AdminController(AdminService adminService , PasswordEncoder passwordEncoder) {
 		this.adminService = adminService;
+		this.passwordEncoder=passwordEncoder;
 	}
 
 	@GetMapping("/admin-dashboard")
@@ -71,5 +79,37 @@ public class AdminController {
 	public String updateAdmin(@ModelAttribute("admin") Admin admin){
 		adminService.updateAdmin(admin);
 		return "redirect:/admin-profile";		
+	}
+	
+	@GetMapping("/admin-operations")
+	public String adminOperations(Model model) {
+		model.addAttribute("admin", new Admin());
+		return "Admin/admin-operations";
+	}
+	
+	@PostMapping("/addAdmin")
+	public String addAdmin( @Valid @ModelAttribute Admin admin,BindingResult bindingResult ,RedirectAttributes redirectAttributes,Model model){
+		
+		if (bindingResult.hasErrors()) {
+            // If there are validation errors, return to the form to display them
+            model.addAttribute("admin", admin); // Re-add the admin object to pre-fill the form
+            return "Admin/admin-operations"; // Assuming "register-admin" is your HTML template name
+        }
+		System.out.println(admin);
+		admin.setPassword(passwordEncoder.encode(admin.getPassword()));
+		Users user = new Users();
+		Authorities authority = new Authorities();
+		
+		user.setUsername(admin.getEmail());
+		user.setPassword(admin.getPassword());
+		user.setEnabled("1");
+		
+		authority.setUsername(admin.getEmail());
+		authority.setAuthority("ROLE_ADMIN");
+		
+		adminService.addAdmin(user, authority, admin);
+        redirectAttributes.addFlashAttribute("successMessage", "Admin added successfully!");
+		return "redirect:/admin-operations";
+		
 	}
 }
