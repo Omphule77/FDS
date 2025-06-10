baaf535fc232c6e58d5ac1b1d4f91ee09de9e2b9
lalit-shelkar <lalitshelkar2424@gmail.com>
2025-06-10 09:03:32 +0530

    admin profile

--- foodiexpress/src/main/java/com/fds/foodiexpress/Service/AdminServiceImpl.java
+++ foodiexpress/src/main/java/com/fds/foodiexpress/Service/AdminServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fds.foodiexpress.dao.AdminDAO;
+import com.fds.foodiexpress.entity.Admin;
 import com.fds.foodiexpress.entity.Customer;
 import com.fds.foodiexpress.entity.Delivery;
 import com.fds.foodiexpress.entity.Restaurant;
@@ -42,6 +43,17 @@ public class AdminServiceImpl implements AdminService {
 	@Transactional
 	public void toggleCustomerFlag(String email) {
 		adminDAO.toggleCustomerFlag(email);
+	}
+
+	@Override
+	public Admin getAdminDetails(String email) {
+		return adminDAO.findByEmail(email);
+	}
+
+	@Override
+	public void updateAdmin(Admin admin) {
+		adminDAO.update(admin);
+		
 	}
 
 	
